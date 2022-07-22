d9bd6a4a87749eb19b65239ab7a3d069f60d5c57
Ignacio Campos Stambuk <stambuck94@gmail.com>
2022-07-22 10:39:18 -0400

    Modify script, add new flag

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
-gcloud compute os-config patch-jobs execute \
-    --instance-filter-names="zones/southamerica-east1-b/instances/pruebabastioncluster" \
-    --description "new patching poc"
+gcloud compute os-config patch-jobs execute 
+    --instance-filter-names=zones/southamerica-east1-b/instances/pruebabastioncluster 
+    --description=patching-poc 
+    --display-name=patching-job-01
